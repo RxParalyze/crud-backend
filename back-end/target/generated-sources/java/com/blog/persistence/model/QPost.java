d3024a4f2f2ec36c2a192286da64feee623f952a
RxParalyze <c17phillip.smart@gmail.com>
2022-01-24 14:04:18 -0500

    moved back-end

---
+++ back-end/target/generated-sources/java/com/blog/persistence/model/QPost.java
@@ -0,0 +1,51 @@
+package com.blog.persistence.model;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QPost is a Querydsl query type for Post
+ */
+@Generated("com.querydsl.codegen.EntitySerializer")
+public class QPost extends EntityPathBase<Post> {
+
+    private static final long serialVersionUID = 1909099507L;
+
+    public static final QPost post = new QPost("post");
+
+    public final NumberPath<Integer> authorId = createNumber("authorId", Integer.class);
+
+    public final StringPath content = createString("content");
+
+    public final DateTimePath<java.time.LocalDateTime> createdAt = createDateTime("createdAt", java.time.LocalDateTime.class);
+
+    public final StringPath excerpt = createString("excerpt");
+
+    public final NumberPath<Integer> id = createNumber("id", Integer.class);
+
+    public final BooleanPath published = createBoolean("published");
+
+    public final StringPath title = createString("title");
+
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = createDateTime("updatedAt", java.time.LocalDateTime.class);
+
+    public QPost(String variable) {
+        super(Post.class, forVariable(variable));
+    }
+
+    public QPost(Path<? extends Post> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QPost(PathMetadata metadata) {
+        super(Post.class, metadata);
+    }
+
+}
+
---
+++ back-end/target/generated-sources/java/com/blog/persistence/model/QUser.java
@@ -0,0 +1,49 @@
+package com.blog.persistence.model;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QUser is a Querydsl query type for User
+ */
+@Generated("com.querydsl.codegen.EntitySerializer")
+public class QUser extends EntityPathBase<User> {
+
+    private static final long serialVersionUID = 1909251870L;
+
+    public static final QUser user = new QUser("user");
+
+    public final DateTimePath<java.time.LocalDateTime> createdAt = createDateTime("createdAt", java.time.LocalDateTime.class);
+
+    public final StringPath firstName = createString("firstName");
+
+    public final StringPath hash = createString("hash");
+
+    public final NumberPath<Integer> id = createNumber("id", Integer.class);
+
+    public final StringPath lastName = createString("lastName");
+
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = createDateTime("updatedAt", java.time.LocalDateTime.class);
+
+    public final StringPath userName = createString("userName");
+
+    public QUser(String variable) {
+        super(User.class, forVariable(variable));
+    }
+
+    public QUser(Path<? extends User> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QUser(PathMetadata metadata) {
+        super(User.class, metadata);
+    }
+
+}
+
